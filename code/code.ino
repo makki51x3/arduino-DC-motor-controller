ff0d7dd33185859f360d78a58d3b9f1e3b1e3387
makki51x3 <96151955+makki51x3@users.noreply.github.com>
2022-02-03 07:29:04 -0800

    Add files via upload

--- code/code.ino
+++ code/code.ino
@@ -1,64 +1,32 @@
-#include <Servo.h>
+int state; 
+void setup() { 
+  Serial.begin(9600); 
+  pinMode(A1,INPUT); 
+  pinMode(5,OUTPUT); 
+} 
 
-Servo motor;
-
-float ldr1, ldr2, ldr3;
-
-void setup() {
-
-motor.attach(10);
-
-Serial.begin(9600);
-
-motor.write(0);
-
-}
-
-void loop() {
-
-//Reading the sensors’ inputs
-
-ldr1 = analogRead(A0);
-
-ldr2 = analogRead(A1);
-
-ldr3 = analogRead(A2);
-
-Serial.print("Sensor 1 is reading: "); Serial.println(ldr1);
-
-delay(100);
-
-Serial.print("Sensor 2 is reading: "); Serial.println(ldr2);
-
-delay(100);
-
-Serial.print("Sensor 3 is reading: "); Serial.println(ldr3);
-
-delay(100);
-
-Serial.println("-----------------------------------------");
-
-//Determining which sensor to turn to if(ldr1 > max(ldr2, ldr3)){
-
-motor.write(150);
-
-delay(15);
-
-}
-
-if(ldr2 > max(ldr1, ldr3)){
-
-motor.write(90);
-
-delay(15);
-
-}
-
-if(ldr3 > max(ldr1, ldr2)){
-
-motor.write(30);
-
-delay(15);
-
-}
+void loop() { 
+  if(Serial.available()>0){ 
+    int x=Serial.read(); 
+    switch(x){
+      case 'a': 
+      state=0; 
+      Serial.println("state is 0"); 
+      break; 
+      
+      case 'b': 
+      state=1; 
+      Serial.println("state is 1"); 
+      break; 
+      }
+      } 
+  if(state==0){ 
+    digitalWrite(5,LOW); 
+    Serial.println("motor is off"); 
+    } 
+    
+  if(state==1){ 
+    analogWrite(5,analogRead(A1)/4); 
+    Serial.println("motor is reading"); 
+    } 
 }
